2fd813e4cc09ec26dfd8d7140835b8264bf52889
Vlad Sivakov <svv@salesap.ru>
2023-08-06 22:54:31 +0300

    PhotoManager

---
+++ app/api/image-manager/route.js
@@ -0,0 +1,21 @@
+import { writeFile } from 'fs/promises'
+import { NextRequest, NextResponse } from 'next/server'
+const path = require("path");
+
+export async function POST(request) {
+  const data = await request.formData()
+  const file = data.get('image');
+
+  if (!file) {
+    return NextResponse.json({ success: false })
+  }
+
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
+
+  const pathImage = path.resolve(`./public/image-manager/${file.name}`)
+  await writeFile(pathImage, buffer)
+  console.log(`open ${pathImage} to see the uploaded file`)
+
+  return NextResponse.json({ success: true })
+}
---
+++ app/api/route.js
@@ -0,0 +1,25 @@
+// app/api/route.js 👈🏽
+
+import { NextResponse } from "next/server";
+// import axios from 'axios';
+import fsPromises from 'fs/promises';
+
+export async function GET(request) {
+  // Do whatever you want
+  const folder = './public/image-manager';
+  const jsonData = await fsPromises.readdir(folder)
+
+  const url = new URL(request.url);
+  const token = url.searchParams.get("token");
+  const idDeals = url.searchParams.get("ids[]");
+  const userId = url.searchParams.get("user_id");
+  const alone = url.searchParams.get("alone");
+
+  const images = jsonData.filter(e => e.includes(idDeals));
+
+
+  // console.log(c);
+  // https://vs113.ru/right_menu?hash=38c4af65&ids=&token=O0ApruzXzguScRieilPQnUOPgGnK0BHHvmNgZzit3_g&type=users&user_id=76790
+  // https://example.com?ids%5B%5D=7272721&token=O0ApruzXzguScRieilPQnUOPgGnK0BHHvmNgZzit3_g&type=deals&user_id=76790
+  return NextResponse.json({ data: images }, { status: 200 });
+}
