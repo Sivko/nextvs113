d339414fc229d9298113f57529529e92ad500c07
Vlad Sivakov <svv@salesap.ru>
2023-08-09 12:08:12 +0300

    Hard UPD

--- app/api/v1/[...params]/route.js
+++ app/api/v1/[...params]/route.js
@@ -8,8 +8,8 @@ export async function POST(req) {
 
   const serilizeUrl = req.url.split('/')
 
-  const token = serilizeUrl[serilizeUrl.length-1]
-  const scriptId = serilizeUrl[serilizeUrl.length-2]
+  const token = serilizeUrl[serilizeUrl.length - 1]
+  const scriptId = serilizeUrl[serilizeUrl.length - 2]
 
   const options = {
     headers: {
@@ -18,23 +18,31 @@ export async function POST(req) {
       // 'Accept': '*'
     }
   }
-  const data = req.body.data
-  let newData = { "type": "companies", "id": data.id, "attributes": { "customs": {}, "allow-websockets": true } };
-  let constants = await axios.get('https://app.salesap.ru/api/v1/constants', options).catch((err) => res.json(err));
+
+  // newData.type = "deals";
+  // let text = rubles(data.data.custom_101206);
+  // if (text) { newData.attributes.customs.custom_103046 = text.charAt(0).toUpperCase() + text.slice(1) } 
+  // else { newData.attributes.customs.custom_103046 = 0 } return newData
+
+  const tmp = await req.json()
+  const data = tmp.data;
+  let newData = { "type": "companies", "id": data.id, "attributes": { "customs": {}} };
+  let constants = await axios.get('https://app.salesap.ru/api/v1/constants', options).catch((err) => NextResponse.json(err));
   let constant = constants.data.data;
   let webScript = constant.find(obj => obj.id == scriptId).attributes.value
 
   let compute = new Function('data', 'newData', 'rubles', webScript);
-  const resEnd = await axios.patch('https://app.salesap.ru/api/v1/' + compute(data, newData, rubles).type + '/' + data.id, JSON.stringify({ "data": compute(data, newData, rubles) }), options).catch((err) => res.json(err))
-
-  return NextResponse.json({ success: resEnd })
+  const resEnd = await axios.patch('https://app.salesap.ru/api/v1/' + compute(data, newData, rubles).type + '/' + data.id, JSON.stringify({ "data": compute(data, newData, rubles) }), options).catch((err) => NextResponse.json(err))
+  // const resEnd1 = await resEnd.json()
+  // console.log(resEnd1)
+  return NextResponse.json({ success: compute(data, newData, rubles) })
 }
 
 export async function GET(req) {
   const serilizeUrl = req.url.split('/')
-  console.log(serilizeUrl[serilizeUrl.length-1])
-  console.log(serilizeUrl[serilizeUrl.length-2])
-    const axios = require('axios');
+  console.log(serilizeUrl[serilizeUrl.length - 1])
+  console.log(serilizeUrl[serilizeUrl.length - 2])
+  const axios = require('axios');
   const rubles = require('rubles').rubles;
-  return NextResponse.json({ success: "Это Get запрос, а для выполнения скрипта нужен POST :)"})
+  return NextResponse.json({ success: "Это Get запрос, а для выполнения скрипта нужен POST :)" })
 }
